1658e12b500a541b18844109e388cb95829de499
heyuan2020 <1025490067@qq.com>
2020-03-07 11:48:53 +0800

    TieNiu KeTang git

---
+++ ChuanCan/src/cn/tieniu/NiuKeTang.java
@@ -0,0 +1,23 @@
+package cn.tieniu;
+
+public class NiuKeTang {
+
+	public static void main(String[] args) {
+		
+		ketang(niu(5));
+	}
+
+	private static void ketang(int itemId) {
+		itemId+=1;
+		System.out.println(itemId); 
+		
+	}
+
+	private static int niu(int id) {
+		
+		
+		return id;
+	}
+	
+
+}
